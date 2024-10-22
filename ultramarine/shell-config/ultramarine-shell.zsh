bdd7117b152f0cf17ee4e3903898d137ed3d66ad
Danish Rehman <95931540+amethystgosling163@users.noreply.github.com>
2024-10-22 10:53:10 +0500

    feat(shell-config): enable FZF keybinds for FZF reverse history search with Ctrl-R (#81)
    * Update ultramarine-shell.zsh
    
    * Update ultramarine-shell-config.spec

--- ultramarine/shell-config/ultramarine-shell.zsh
+++ ultramarine/shell-config/ultramarine-shell.zsh
@@ -6,6 +6,8 @@ eval "$(starship init zsh)"
 source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
+source /usr/share/fzf/shell/key-bindings.zsh
+
 # Ctrl + Arrow keybindings
 bindkey "^[[1;5D" backward-word
 bindkey "^[[1;5C" forward-word
