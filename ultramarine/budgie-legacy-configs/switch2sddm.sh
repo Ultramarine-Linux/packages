6e9d1df3e073414572af902d37d6e137cbb3d2b8
Jaiden Riordan <jade@fyralabs.com>
2026-05-29 14:36:22 -0500

    natural stupidity strikes again

--- ultramarine/budgie-legacy-configs/switch2sddm.sh
+++ ultramarine/budgie-legacy-configs/switch2sddm.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 CHECKFILE="/etc/sddm-switched"
 
 if [[ -f "$CHECKFILE" ]]; then
-    echo "You're already on SDDM, if you're still getting the notification, get support or file an issue.."
-    read -p "Press Enter to open Community page on the Wiki, or Ctrl+C to exit" && xdg-open "https://wiki.ultramarine-linux/en/community/community.org"
+    echo "You're already on SDDM, if you're still getting the notification, get support or file an issue."
+    read -p "Press Enter to open Community page on the Wiki, or Ctrl+C to exit" && xdg-open "https://wiki.ultramarine-linux.org/en/community/community"
     exit 0
 fi
 
@@ -21,7 +21,7 @@ fi
 
 if [[ "$DM" != "lightdm" ]]; then
     echo "You're not on lightdm, seeing '${DM:-unknown}'. If you know what you're doing stop here, otherwise get help on Discord." >&2
-    read -p "Press Enter to open Community page on the Wiki, or Ctrl+C to exit" && xdg-open "https://wiki.ultramarine-linux/en/community/community.org"
+    read -p "Press Enter to open Community page on the Wiki, or Ctrl+C to exit" && xdg-open "https://wiki.ultramarine-linux.org/en/community/community"
     exit 1
 fi
 
