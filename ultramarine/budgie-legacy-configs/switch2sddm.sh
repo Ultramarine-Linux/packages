249a598ee0547b61fe3c8d00893645e5ca103d82
Jaiden Riordan <jade@fyralabs.com>
2026-05-29 14:31:20 -0500

    add discord links

--- ultramarine/budgie-legacy-configs/switch2sddm.sh
+++ ultramarine/budgie-legacy-configs/switch2sddm.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 CHECKFILE="/etc/sddm-switched"
 
 if [[ -f "$CHECKFILE" ]]; then
-    echo "You're already on SDDM, if you're still getting the notification get help on Discord."
+    echo "You're already on SDDM, if you're still getting the notification, get support or file an issue.."
+    read -p "Press Enter to open Community page on the Wiki, or Ctrl+C to exit" && xdg-open "https://wiki.ultramarine-linux/en/community/community.org"
     exit 0
 fi
 
@@ -20,7 +21,7 @@ fi
 
 if [[ "$DM" != "lightdm" ]]; then
     echo "You're not on lightdm, seeing '${DM:-unknown}'. If you know what you're doing stop here, otherwise get help on Discord." >&2
-    echo "Aborting" >&2
+    read -p "Press Enter to open Community page on the Wiki, or Ctrl+C to exit" && xdg-open "https://wiki.ultramarine-linux/en/community/community.org"
     exit 1
 fi
 
@@ -36,4 +37,4 @@ pkexec touch "$CHECKFILE"
 echo ""
 echo "All Done!"
 echo "You need to reboot, save your work now, then reboot when you're ready"
-read -p "Press enter to reboot"
+read -p "Press enter to reboot" && pkexec reboot
